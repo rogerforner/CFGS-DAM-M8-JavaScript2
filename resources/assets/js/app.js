52070f3091e684cb256d70621132b3924fcf8e02
Roger Forner Fabre <sir.rogerforner@gmail.com>
2016-10-19 19:31:00 +0200

    JQuey Ajax

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -19,18 +19,6 @@ const app = new Vue({
     el: '#app'
 });
 
-
-var animals = require('./animal');
-var gossos = require('./dog');
-var gats = require('./cat');
-
-//
-//     And this is how your scenario plays out:
-
-var cat = new gats.Cat();
-var dog = new gossos.Dog();
-
-cat.eat();
-dog.eat();
-cat.say();
-dog.say();
+//jQuery.ajax(); Executa un XHR de JS utilitzant jquery
+//http://api.jquery.com/jquery.ajax/
+$.ajax();
